9c78239a58c6d697326ed66a53f6f828e01cdfd6
Celso Yamashita <ctyamashita@hotmail.com>
2022-05-23 14:55:55 +0900

    routes initial setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :places, except: %i[destroy edit update] do
+    resources :photos, only: %i[create]
+    resources :lists, only: :update
+  end
+
+  resources :lists, only: %i[create index]
 end
