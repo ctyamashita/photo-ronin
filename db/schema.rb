1299ce02b6a0c09232a6d5e5b9da6b689ffcc24e
Celso Yamashita <ctyamashita@hotmail.com>
2022-05-24 11:46:37 +0900

    migration file created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_23_085950) do
+ActiveRecord::Schema.define(version: 2022_05_24_024034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,7 +73,6 @@ ActiveRecord::Schema.define(version: 2022_05_23_085950) do
   end
 
   create_table "photos", force: :cascade do |t|
-    t.string "image_url"
     t.bigint "place_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
