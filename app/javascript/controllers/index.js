87f7ac30129f96f1bb83ac773e661b8c4d2942ea
Celso Yamashita <ctyamashita@hotmail.com>
2022-05-24 22:57:03 +0900

    correction to map

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,14 +2,14 @@
 // Controller files must be named *_controller.js.
 
 import { Application } from "stimulus"
+// import { Application } from '@hotwired/stimulus'
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+// import Carousel from 'stimulus-carousel'
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
 
-import { Application } from '@hotwired/stimulus'
-import Carousel from 'stimulus-carousel'
 
-const application = Application.start()
-application.register('carousel', Carousel)
+// application.register('carousel', Carousel)
--- app/javascript/controllers/map_toggle_controller.js
+++ app/javascript/controllers/map_toggle_controller.js
@@ -9,12 +9,12 @@ export default class extends Controller {
   map_display() {
     this.mapTarget.classList.remove('d-none')
     this.listTarget.classList.add('d-none')
-    this.buttonTarget.innerHTML = '<i class="icon-big gray fas fa-bars" data-action="click->map-toggle#list_display" data-map-toggle-target="button"></i>'
+    this.buttonTarget.innerHTML = '<i class="icon-big gray fas fa-bars" data-action="click->map-toggle#list_display"></i>'
   }
 
   list_display() {
     this.mapTarget.classList.add('d-none')
     this.listTarget.classList.remove('d-none')
-    this.buttonTarget.innerHTML = '<i class="icon-big gray fas fa-map" data-action="click->map-toggle#map_display" data-map-toggle-target="button"></i>'
+    this.buttonTarget.innerHTML = '<i class="icon-big gray fas fa-map" data-action="click->map-toggle#map_display"></i>'
   }
 }
