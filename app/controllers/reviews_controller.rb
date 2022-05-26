2025f7d903f445b200909a846e5491e465273e3a
Nana kadode <amar.nana5048@gmail.com>
2022-05-26 15:47:08 +0900

    added def create for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,20 @@
 class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    @place = Place.find(params[:place_id])
+    @review.place = @place
+    @review.user = current_user
+    authorize @review
+
+    if @review.save
+      redirect_to place_path(@place)
+    else
+      render 'place/reviews'
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
 end
