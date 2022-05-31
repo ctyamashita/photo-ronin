084b2285698ac326e913de8e8648fca040e2ad02
Hakuyo Harimoto <haharimoto@gmail.com>
2022-05-31 13:18:34 +0900

    added real recommended and trending on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,27 @@ class PagesController < ApplicationController
 
   def home
     @places = Place.all
+    # TO DO
+    # get average rating from place model
+    # sort each place instance by rating (descending order)
+
+    # TO DO
+    # assign amount of markers to each place instance (using hash)
+    # sort by highest amount of markers
+    @recommended = @places.sort_by { |place| -place.average_review }
+    @trending = @places.sort_by { |place| -place.markers.count }
+
+    # same thing as @trending above
+    # place_with_marker =
+    #   @places.map do |place|
+    #     {
+    #       name: place.name,
+    #       address: place.address,
+    #       latitude: place.latitude,
+    #       longitude: place.longitude,
+    #       markers_num: place.markers.count
+    #     }
+    #   end
+    # @trending = place_with_marker.sort_by { |place| -place[:markers_num] }
   end
 end
